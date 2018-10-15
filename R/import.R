1fc88d86680ce53d93f78a3b9cb657f07cdda4d3
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2018-10-15 17:16:14 +0200

    import: vertices incl. positions and directed vertices get caught correctly

--- R/import.R
+++ R/import.R
@@ -1,17 +1,20 @@
 vertices <- function(mdl) {
-  mdl <- mdl[as.numeric(sapply(mdl, function(x){grep("A FUNCTION OF", x)})), ]
-  labels <- trimws(sapply(strsplit(mdl, "= A FUNCTION OF"), function(x){x[1]}))
+  mdl <- filter_functional_definitions(mdl)
+  labels <- lhs(mdl, "= A FUNCTION OF")
   data.frame(id = 1:length(labels), label = labels, stringsAsFactors = FALSE)
 }
 
 edges <- function(mdl) {
-  mdl <- mdl[as.numeric(sapply(mdl, function(x){grep("A FUNCTION OF", x)})), ]
   vertices <- vertices(mdl)
-  to <- trimws(sapply(strsplit(mdl, "= A FUNCTION OF"), function(x){x[1]}))
+  mdl <- filter_functional_definitions(mdl)
+  to <- lhs(mdl, "= A FUNCTION OF")
   to <- sapply(to, function(x){vertices$id[vertices$label == x]})
 
-  from <- trimws(gsub("[()]", " ", sapply(strsplit(mdl, "= A FUNCTION OF"), function(x){x[2]})))
+  from <- rhs(mdl, "= A FUNCTION OF")
   from <- from[from != ""]
+  from <- strsplit(from, ",")
+  to <- rep(to, lengths(from))
+  from <- unlist(from)
   from <- sapply(from, function(x){vertices$id[vertices$label == x]})
 
   edges <- data.frame(from = from, to = to, stringsAsFactors = FALSE)
@@ -20,4 +23,40 @@ edges <- function(mdl) {
 
 read_mdl <- function(file) {
   read.csv(file, sep = ";", stringsAsFactors = FALSE)
+}
+
+filter_functional_definitions <- function(mdl) {
+  mdl[as.numeric(sapply(mdl, function(x){grep("A FUNCTION OF", x)})), ]
+}
+
+xhs <- function(element) {
+  function(data, sep) {
+    trimws(gsub("[()]", " ", sapply(strsplit(data, sep), function(x){x[element]})))
+  }
+}
+
+lhs <- xhs(1)
+rhs <- xhs(2)
+
+#' Title
+#'
+#' @param mdl
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' positions(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl"))
+#' positions(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))
+#' positions(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))
+positions <- function(mdl) {
+  vertices <- vertices(mdl)
+  mdl <- mdl[sapply(mdl, function(x){grepl(paste(vertices$label, collapse = "|"), x)}), ]
+  mdl <- mdl[!grepl("A FUNCTION OF", mdl)]
+  names <- sapply(strsplit(mdl, ","), function(x)x[[3]])
+  x <- as.numeric(sapply(strsplit(mdl, ","), function(x)x[[4]]))
+  y <- -as.numeric(sapply(strsplit(mdl, ","), function(x)x[[5]]))
+  df <- data.frame(label = names, x = x, y = y, stringsAsFactors = FALSE)
+  return(merge(vertices, df)[c("x", "y")]
+  )
 }
