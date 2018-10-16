2bf8d71bb4071c5890423d9884c85d5dda9af15d
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2018-10-16 11:41:08 +0200

    add and show groups works!

--- R/plot.R
+++ R/plot.R
@@ -48,15 +48,20 @@ add_polarities <- function(cld_igraph, polarity) {
 #' @export
 #'
 #' @examples
-#' g <- create_igraph(vertices(read_mdl("tests/testthat/mdl/cld-adoption.mdl")), edges(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))) %>%
-#'   add_positions(positions(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))) %>% add_polarities(polarities(read_mdl("tests/testthat/mdl/cld-adoption.mdl")))
-#' plot_cld(g)
 #' g <- create_igraph(vertices(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl")), edges(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl"))) %>%
 #'   add_positions(positions(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl"))) %>% add_polarities(polarities(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl")))
 #' plot_cld(g)
 #' g <- create_igraph(vertices(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl")), edges(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))) %>%
 #'   add_positions(positions(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))) %>% add_polarities(polarities(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl")))
 #' plot_cld(g)
+#' g <- create_igraph(vertices(read_mdl("tests/testthat/mdl/cld-adoption.mdl")), edges(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))) %>%
+#'   add_positions(positions(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))) %>% add_polarities(polarities(read_mdl("tests/testthat/mdl/cld-adoption.mdl")))
+#' plot_cld(g)
+#' plot_cld(g) + geom_node_text(aes(label = name, x = x, y = y), color = 'red')
+#' g <- g %>% add_group(flows = adoption_rate)
+#' g <- g %>% add_group(stocks = potential_adopters + adopters)
+#' plot_cld(g) + show_group(group = "stocks")
+#' plot_cld(g) + show_group(group = "flows")
 plot_cld <- function(cld_igraph) {
   library(ggraph)
   ggraph(cld_igraph, layout = 'manual', node.positions = data.frame(x = igraph::V(cld_igraph)$x, y = igraph::V(cld_igraph)$y)) +
@@ -67,4 +72,9 @@ plot_cld <- function(cld_igraph) {
                   angle_calc = 'along',
                   label_dodge = unit(2.5, 'mm'),
                   arrow = arrow(length = unit(4, 'mm')))
+}
+
+show_group <- function(mapping = NULL, data = NULL, group, color = 'red') {
+  color <- ifelse(igraph::get.vertex.attribute(g, paste0("group_", group)), color, 'black')
+  geom_node_text(aes(label = name, x = x, y = y), color = color)
 }
