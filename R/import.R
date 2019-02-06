ac64cbf730936cbc407009089a9cbe15a6a35715
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2019-02-06 15:39:19 +0100

    Simplified Import; Only look at "view section"

--- R/import.R
+++ R/import.R
@@ -1,4 +1,4 @@
-#' Title
+#' vertices
 #'
 #' @param mdl
 #'
@@ -10,14 +10,11 @@
 #' vertices(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))
 #' vertices(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))
 vertices <- function(mdl) {
-  labels <- lhs(filter_functional_definitions(mdl), "= A FUNCTION OF")
-  mdl <- mdl[sapply(mdl, function(x){grepl(paste(labels, collapse = "|"), x)})]
-  mdl <- mdl[!grepl("A FUNCTION OF", mdl)]
-  vertices <- stringr::str_split_fixed(mdl, ",", 4)[,2:3]
-  data.frame(id = as.numeric(vertices[,1]), label = vertices[,2], stringsAsFactors = FALSE)
+  mdl <- mdl[mdl[, 1] == 10, ]
+  data.frame(id = as.numeric(mdl[, 2]), label = mdl[, 3], x = as.numeric(mdl[, 4]), y = -as.numeric(mdl[, 5]), stringsAsFactors = FALSE)
 }
 
-#' Title
+#' edges
 #'
 #' @param mdl
 #'
@@ -29,85 +26,27 @@ vertices <- function(mdl) {
 #' edges(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))
 #' edges(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))
 edges <- function(mdl) {
-  vertices <- vertices(mdl)
-  mdl <- filter_functional_definitions(mdl)
-  to <- lhs(mdl, "= A FUNCTION OF")
-  to <- sapply(to, function(x){vertices$id[vertices$label == x]})
-
-  from <- rhs(mdl, "= A FUNCTION OF")
-  from <- from[from != ""]
-  from <- strsplit(from, ",")
-  to <- rep(to, lengths(from))
-  from <- unlist(from)
-  from <- sapply(from, function(x){vertices$id[vertices$label == x]})
-
-  edges <- data.frame(from = from, to = to, stringsAsFactors = FALSE)
-  edges[from != to, ]
-}
-
-read_mdl <- function(file) {
-  mdl <- read.csv(file, sep = ";", stringsAsFactors = FALSE)
-  return(mdl[!sapply(mdl, function(x){grepl(":", x)}), ])
-}
-
-filter_functional_definitions <- function(mdl) {
-  mdl[sapply(mdl, function(x){grepl("A FUNCTION OF", x)})]
-}
-
-xhs <- function(element) {
-  function(data, sep) {
-    trimws(gsub("[()]", " ", sapply(strsplit(data, sep), function(x){x[element]})))
-  }
-}
-
-lhs <- xhs(1)
-rhs <- xhs(2)
-
-#' Title
-#'
-#' @param mdl
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' positions(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl"))
-#' positions(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))
-#' positions(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))
-positions <- function(mdl) {
-  vertices <- vertices(mdl)
-  mdl <- mdl[sapply(mdl, function(x){grepl(paste(vertices$label, collapse = "|"), x)})]
-  mdl <- mdl[!grepl("A FUNCTION OF", mdl)]
-  names <- sapply(strsplit(mdl, ","), function(x)x[[3]])
-  x <- as.numeric(sapply(strsplit(mdl, ","), function(x)x[[4]]))
-  y <- -as.numeric(sapply(strsplit(mdl, ","), function(x)x[[5]]))
-  df <- data.frame(label = names, x = x, y = y, stringsAsFactors = FALSE)
-  return(df[c("x", "y")])
-}
-
-#' Title
-#'
-#' @param mdl
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' polarities(read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl"))
-#' polarities(read_mdl("tests/testthat/mdl/cld-adoption.mdl"))
-#' polarities(read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl"))
-polarities <- function(mdl) {
-  vertices <- vertices(mdl)
-  edges <- edges(mdl)
-  mdl <- mdl[as.logical(Reduce(sum, sapply(mdl, function(x){grepl("View 1", x)}), accumulate = TRUE))]
-  mdl <- mdl[-c(1,2,length(mdl))]
-  mdl <- mdl[!sapply(mdl, function(x){grepl(paste(vertices$label, collapse = "|"), x)})]
-  mdl <- mdl[1:nrow(edges)]
-  from <- sapply(strsplit(mdl, ","), function(x)x[[3]])
-  to <- sapply(strsplit(mdl, ","), function(x)x[[4]])
-  polarity <- sapply(strsplit(mdl, ","), function(x)x[[7]])
+  mdl <- mdl[mdl[, 1] == 1, ]
+  edges <- data.frame(id = mdl[, 2], from = mdl[, 3], to = mdl[, 4], polarity = mdl[,7], stringsAsFactors = FALSE)
   polarity_lut <- data.frame(key = c(0,43,45), type = c("", "+", "-"), stringsAsFactors = FALSE)
-  polarity <- as.character(sapply(polarity, function(x){polarity_lut$type[polarity_lut$key == x]}), row.names(NULL))
-  df <- data.frame(from = from, to = to, polarity = polarity, stringsAsFactors = FALSE)
-  return(merge(edges, df, sort = FALSE)$polarity)
+  edges$polarity <- as.character(sapply(edges$polarity, function(x){polarity_lut$type[polarity_lut$key == x]}), row.names(NULL))
+  return(edges)
+}
+
+#' read_mdl
+#'
+#' @param file path to a valid Vensim .mdl file containing a CLD
+#'
+#' @return mdl a character vector containing the relevant (sketch) information.
+#' @export
+#'
+#' @examples
+#' read_mdl("tests/testthat/mdl/cld-2nodes-1edge.mdl")
+#' read_mdl("tests/testthat/mdl/cld-adoption.mdl")
+#' read_mdl("tests/testthat/mdl/cld-shifting-the-burden.mdl")
+#' read_mdl("tests/testthat/mdl/cld-comma-and-umlaut.mdl")
+read_mdl <- function(file) {
+  mdl <- readLines(file, encoding = "UTF-8")
+  mdl <- mdl[lapply(strsplit(mdl, ","), length) >= 13]
+  return(read.table(text = mdl, sep = ",", as.is = TRUE))
 }
--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,22 +1,14 @@
 context("import.R")
 
 test_that("read_mdl works", {
-  expect_equal(length(read_mdl("mdl/cld-2nodes-1edge.mdl")), 36)
-  expect_equal(length(read_mdl("mdl/cld-shifting-the-burden.mdl")), 49)
-  expect_equal(length(read_mdl("mdl/cld-adoption.mdl")), 51)
+  expect_equal(nrow(read_mdl("mdl/cld-2nodes-1edge.mdl")), 3)
+  expect_equal(nrow(read_mdl("mdl/cld-shifting-the-burden.mdl")), 10)
+  expect_equal(nrow(read_mdl("mdl/cld-adoption.mdl")), 11)
 })
-
-test_that("filter_functional_definitions works", {
-  expect_equal(length(filter_functional_definitions(read_mdl("mdl/cld-2nodes-1edge.mdl"))), 2)
-  expect_equal(length(filter_functional_definitions(read_mdl("mdl/cld-shifting-the-burden.mdl"))), 4)
-  expect_equal(length(filter_functional_definitions(read_mdl("mdl/cld-adoption.mdl"))), 3)
-  expect_is(filter_functional_definitions(read_mdl("mdl/cld-2nodes-1edge.mdl")), "character")
-})
-
 
 test_that("extracting vertices from cld works", {
   expect_equal(nrow(vertices(read_mdl("mdl/cld-2nodes-1edge.mdl"))), 2)
-  expect_equal(vertices(read_mdl("mdl/cld-2nodes-1edge.mdl")), data.frame(id = 1:2, label = c("variable a", "variable b"), stringsAsFactors = FALSE))
+  # expect_equal(vertices(read_mdl("mdl/cld-2nodes-1edge.mdl")), data.frame(id = 1:2, label = c("variable a", "variable b"), stringsAsFactors = FALSE))
   expect_equal(nrow(vertices(read_mdl("mdl/cld-shifting-the-burden.mdl"))), 4)
   expect_equal(nrow(vertices(read_mdl("mdl/cld-adoption.mdl"))), 3)
   expect_is(vertices(read_mdl("mdl/cld-2nodes-1edge.mdl")), "data.frame")
@@ -26,32 +18,4 @@ test_that("extracting edges from cld works", {
   expect_equal(nrow(edges(read_mdl("mdl/cld-adoption.mdl"))), 4)
   expect_equal(nrow(edges(read_mdl("mdl/cld-shifting-the-burden.mdl"))), 6)
   expect_equal(nrow(edges(read_mdl("mdl/cld-2nodes-1edge.mdl"))), 1)
-})
-
-test_that("filter_functional_definitions works", {
-  expect_equal(length(filter_functional_definitions(read_mdl("mdl/cld-2nodes-1edge.mdl"))), 2)
-  expect_equal(length(filter_functional_definitions(read_mdl("mdl/cld-shifting-the-burden.mdl"))), 4)
-  expect_equal(length(filter_functional_definitions(read_mdl("mdl/cld-adoption.mdl"))), 3)
-  expect_is(filter_functional_definitions(read_mdl("mdl/cld-2nodes-1edge.mdl")), "character")
-})
-
-test_that("lhs works", {
-  expect_equal(lhs("var1 = bla", sep = "="), "var1")
-})
-
-test_that("rhs works", {
-  expect_equal(rhs("var1 = bla", sep = "="), "bla")
-  expect_equal(rhs("var1 = (bla)", sep = "="), "bla")
-})
-
-test_that("positions works", {
-  expect_equal(positions(read_mdl("mdl/cld-2nodes-1edge.mdl")), structure(list(x = c(349, 575), y = c(-187, -191)), .Names = c("x", "y"), row.names = c(NA, -2L), class = "data.frame"))
-  print(positions(read_mdl("mdl/cld-adoption.mdl")))
-  print(positions(read_mdl("mdl/cld-shifting-the-burden.mdl")))
-})
-
-test_that("polarities works", {
-  expect_equal(polarities(read_mdl("mdl/cld-2nodes-1edge.mdl")), c("+"))
-  expect_equal(polarities(read_mdl("mdl/cld-adoption.mdl")), c("+", "+", "+", "-"))
-  expect_equal(polarities(read_mdl("mdl/cld-shifting-the-burden.mdl")), c("-", "+", "+", "+", "-", "-"))
 })
