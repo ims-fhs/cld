f87dff84266cdf6f940dad2fc1cfc550c343b72f
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2019-04-10 08:35:15 +0200

    simple mechanism to add labels to a ref_mode

--- R/describe.R
+++ R/describe.R
@@ -11,7 +11,8 @@
 #' cld <- import("tests/testthat/mdl/burnout.mdl")
 #' cld %>% link(`hours` %->%`energy`) %>% describe(type = "text", description = "The more hours you work, the less energy you have.")
 #' cld %>% link(`hours` %->%`energy`) %>% describe(type = "ref_mode", 0/0 %-% 10/0)
-describe <- function(cld, type, description) {
+#' cld %>% link(`hours` %->%`energy`) %>% describe(type = "ref_mode", 0/0 %-% 10/0, labs = "labs(title = \"some title\", x = \"Year\", y = \"Energy\")")
+describe <- function(cld, type, description, labs = "labs(title = \"\", x = \"\", y = \"\")") {
   assertthat::assert_that(type %in% c("text", "ref_mode"), msg = paste0("'type' must be 'text' or 'ref_mode', not '", type, "'."))
   if(!("description" %in% names(cld))) {
     cld$description <- NA
@@ -19,8 +20,8 @@ describe <- function(cld, type, description) {
   if(type == "text") {
     cld <- add_textual_description(cld, description)
   } else {
-    str <- rlang::as_label(rlang::enexpr(description))
-    cld <- add_ref_mode(cld, str)
+    str <- deparse(substitute(description), width.cutoff = 200L)
+    cld <- add_ref_mode(cld, str, labs)
   }
   return(cld)
 }
@@ -31,8 +32,9 @@ add_textual_description <- function(cld, description) {
   return(cld)
 }
 
-add_ref_mode <- function(cld, str) {
+add_ref_mode <- function(cld, str, x = "labs(title = \"\", x = \"\", y = \"\")") {
   ggplot <- ref_mode_to_ggplot(str)
+  ggplot <- paste0(ggplot, " + ", x)
   description <- data.frame(type = "description_ref_mode", description = ggplot, group = max(cld$group), stringsAsFactors = FALSE)
   cld <- merge(cld, description, all = TRUE, sort = FALSE)
   return(cld)
--- tests/testthat/test-describe.R
+++ tests/testthat/test-describe.R
@@ -15,7 +15,7 @@ test_that("add_ref_mode adds a ref mode in ggplot format to a group of elements"
   cld1 <- cld %>% link(`hours` %->%`energy`) %>% describe(type = "ref_mode", 0/0 %-% 10/10)
   expect_equal(nrow(cld1) - nrow(cld), 1)
   expect_equal(cld1$type[nrow(cld1)], "description_ref_mode")
-  expect_equal(cld1$description[nrow(cld1)], "ggplot() + geom_segment(aes(x = 0, y =0, xend = 10, yend = 10))")
+  expect_equal(cld1$description[nrow(cld1)], "ggplot() + geom_segment(aes(x = 0, y =0, xend = 10, yend = 10)) + labs(title = \"\", x = \"\", y = \"\")")
   expect_equal(cld1$group[nrow(cld1)], 2)
   cld2 <- cld %>% link(`hours` %->%`energy`) %>% describe(type = "ref_mode", 0/0 %-% 10/10) %>% describe(type = "text", description = "The more hours you work, the less energy you have.")
   expect_equal(nrow(cld2) - nrow(cld), 2)
