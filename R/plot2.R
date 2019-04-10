3b13a95998f495aa544e5e46989a5a61a9af0321
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2019-04-10 11:36:39 +0200

    plot2: Bugfix data needs to be ONLY a data.frame. No additional classes!

--- R/plot2.R
+++ R/plot2.R
@@ -2,34 +2,16 @@
 # https://cran.r-project.org/web/packages/ggplot2/vignettes/extending-ggplot2.html
 # https://rud.is/books/creating-ggplot2-extensions/demystifying-ggplot2.html
 
-GeomVar <- ggplot2::ggproto("GeomVar", ggplot2::GeomText,
-                   default_aes = ggplot2::aes(x = x, y = y, label = label, colour = "black", size = 4, angle = 0, hjust = 0.5, vjust = 0.5, alpha = NA, family = "Arial", fontface = 1, lineheight = 1.2, length = 10)
-)
-geom_var <- function(mapping = ggplot2::aes(label = label), data = NULL, position = "identity",
-                     ..., parse = FALSE, nudge_x = 0, nudge_y = 0, check_overlap = FALSE,
-                     na.rm = FALSE, show.legend = NA, inherit.aes = TRUE)
-{
-  if (!missing(nudge_x) || !missing(nudge_y)) {
-    if (!missing(position)) {
-      stop("You must specify either `position` or `nudge_x`/`nudge_y`.",
-           call. = FALSE)
-    }
-    position <- ggplot2::position_nudge(nudge_x, nudge_y)
-  }
-  ggplot2::layer(data = data, mapping = mapping, stat = StatVar, geom = GeomVar,
-                 position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-                 params = list(parse = parse, check_overlap = check_overlap,
-                               na.rm = na.rm, ...))
-}
-
-
-
-StatVar <- ggplot2::ggproto("StatVar", ggplot2::Stat,
-                   # compute_group = function(data, scales) {
-                   #   data[chull(data$x, data$y), , drop = FALSE]
-                   # },
-                   compute_group = function(data, scales, length = 20) {
-                     data$label <- sapply(data$label, function(x) {paste0(strwrap(x, width = length), collapse = "\n")})
+StatVar <- ggplot2::ggproto("StatVar",
+                   ggplot2::Stat,
+                   setup_data = function(data, params){
+                     data <- data[data$type == "var", ]
+                   },
+                   compute_group = function(data, scales, length = 5) {
+                     data$label <- sapply(data$label,
+                                          function(x) {paste0(strwrap(x, width = length),
+                                                              collapse = "\n")})
+                     data
                    }
 )
 
@@ -44,8 +26,28 @@ stat_var <- function(mapping = NULL, data = NULL, geom = "var",
 }
 
 
+# include type as a required aesthetic mapping in GeomVar
+GeomVar <- ggplot2::ggproto("GeomVar", ggplot2::GeomText,
+                   required_aes = c("x", "y", "label", "type"),
+                   default_aes = ggplot2::aes(x = x, y = y, label = label, type = type, colour = "black",
+                                     size = 4, angle = 0, hjust = 0.5, vjust = 0.5,
+                                     alpha = NA, family = "Arial", fontface = 1,
+                                     lineheight = 1.2, length = 10))
 
-#' plot_cld2
+
+geom_var <- function(mapping = ggplot2::aes(label = label, type = type), data = NULL, position = "identity",
+                     ..., parse = FALSE, nudge_x = 0, nudge_y = 0, check_overlap = FALSE,
+                     na.rm = FALSE, show.legend = NA, inherit.aes = TRUE)
+{
+  ggplot2::layer(data = data, mapping = mapping, stat = StatVar, geom = GeomVar,
+                 position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+                 params = list(parse = parse, check_overlap = check_overlap,
+                               na.rm = na.rm, ...))
+}
+
+
+
+#' plot
 #'
 #' @param cld a data.frame containing CLD information
 #'
@@ -53,26 +55,13 @@ stat_var <- function(mapping = NULL, data = NULL, geom = "var",
 #' @export
 #'
 #' @examples
-#' # library(cld)
-#' library(ggplot2)
-#' library(magrittr)
-#' cld <- import("tests/testthat/mdl/flexible-arbeitszeiten-part1.mdl")
-#' cld %<>% link(`Belastung, wegen`)
-#' cld %>% link(Belastung %->% Flexibilisierung)
-#' argument(g, "Belastung, wegen" %->% Flexibilisierung) %>% plot_cld()
-#' argument(g, Belastung %->% Flexibilisierung %->% Möglichkeit) %>% plot_cld()
-#' ggplot(v) + geom_var(aes(x, y))
-#' ggplot(cld, aes(x, y)) + geom_var()
-#' ggplot(cld, aes(x, y)) + geom_text(label = label)
-#' cld[cld$type == "var", ] %>% ggplot() + geom_text(aes(x, y), label = label)
-#' ggplot(cld[cld$type == "var", ], aes(x, y)) + geom_var()
-#' ggplot(v, aes(x, y)) + geom_var(length = 10)
-plot_cld2 <- function(cld) {
-  library(ggplot2)
-  ggplot(aes(x, y)) + geom_var()
-}
-
-
+#' cld <- import("tests/testthat/mdl/burnout.mdl")
+#' vars <- cld[cld$type == "var", ]
+#' ggplot(as.data.frame(vars), aes(x, y)) + geom_text(aes(label = label))
+#' ggplot(as.data.frame(cld), aes(x, y)) + geom_var()
+#' plot(cld)
 plot.cld <- function(cld) {
-  hist(1:10)
+  cld <- as.data.frame(cld)
+  library(ggplot2)
+  ggplot(data = cld, aes(x, y)) + geom_var()
 }
