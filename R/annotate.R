f0795c046df868b14cb1491b190c67175bac478b
Adrian Stämpfli <adrian.staempfli@fhsg.ch>
2019-04-15 15:57:13 +0200

    annotate_text(): wrap descriptions at 70 characters.

--- R/annotate.R
+++ R/annotate.R
@@ -45,7 +45,7 @@ annotate_text <- function(gg, cld) {
   col <- cld$division[cld$type == "description_text"]
   text <- cld$description[cld$type == "description_text"]
   assertthat::assert_that(length(text) == 1, msg = paste0("You provided ", length(text), " textual descriptions, whereas annotate_text needs exactly 1 description."))
-  text <- paste0(strwrap(text, width = 50), collapse = "\n")
+  text <- paste0(strwrap(text, width = 70), collapse = "\n")
   return(ggplot2::annotate("text", x = mean(cld$x, na.rm = TRUE), y = min(cld$y, na.rm = TRUE) - 75, label =  text, colour = cp[col], size = 8, family = font_description))
 }
 
